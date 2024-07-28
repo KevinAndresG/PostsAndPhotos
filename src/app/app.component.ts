5d65284d215d0e0e9f12bd35ac6ba89cabf4a265
KevinAndresG <kevinandresg@gmail.com>
2024-07-28 01:26:37 -0500

    Render the cards using an @for cycle and call the CardComponent for each iteration, use the posts and photos Stores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './shared/components/card/card.component';
+import { PostsStore } from './store/posts.store';
+import { PhotosStore } from './store/photos.store';
+import { JsonPipe } from '@angular/common';
+import { EStates } from './core/models/enums/states.enum';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'posts-and-photos';
+export class AppComponent implements OnInit {
+  readonly postsStore = inject(PostsStore);
+  readonly photosStore = inject(PhotosStore);
+  states = EStates;
+
+  ngOnInit() {
+    this.getPosts();
+    this.getPhotos();
+  }
+
+  getPosts() {
+    this.postsStore.getPosts(0);
+  }
+
+  getPhotos() {
+    this.photosStore.getPhotos(0);
+  }
 }
